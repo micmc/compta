db8706d62b58a12a88997d5943ad056d36d60e91
David Micallef <david.micallef@bull.net>
2014-12-19 15:31:30 +0100

    Script to manage by command line compta database

---
+++ compta.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python
+# -*- coding: utf8 -*-
+
+import optparse
+import sys     
+import os      
+
+from datetime import datetime
+
+from xml.etree.ElementTree import ElementTree
+
+from sqlalchemy import create_engine, select, func, and_, desc
+from sqlalchemy.orm import sessionmaker
+
+from compta.db.banque import Banque
+from compta.db.compte import Compte
+from compta.db.ecriture import Ecriture, EcritureCategorie
+from compta.db.categorie import Categorie
+
+def arg_parse():
+    parser = optparse.OptionParser()                  
+    
+    parser.add_option("-i", "--index",                
+                      help="Compte index to add",     
+                      dest="compte_index", type="int")
+
+    group = optparse.OptionGroup(parser, "List")
+    group.add_option("--solde", action="store_true",
+                     dest="solde", help=U"Solde du compte")
+    group.add_option("--last",  action="store_true",
+                     dest="last", help=U"les 5 dernières opérations")
+    group.add_option("--banque", action="store_true",
+                     dest="banque", help=U"Liste des banques")
+    group.add_option("--compte", action="store_true",
+                     dest="compte", help=U"Liste des comptes")
+    parser.add_option_group(group)
+                                                      
+    (options, args) = parser.parse_args()
+
+    #if options.compte_index is None and not options.compte:  
+    #    print "options -i is missing" 
+    #    sys.exit(1)                   
+
+    return options
+
+def liste_compte(session):
+    comptes = session.query(Compte).all()
+    for compte in comptes:
+        print U"%d : %s, %s - %s" % (compte.id, compte.type, compte.banque.nom, compte.nom)
+
+def liste_banque(session):
+    banques = session.query(Banque).all()
+    for banque in banques:
+        print U"%d : %s" % (banque.id, banque.nom)
+
+def liste_solde(session, compte_id):
+    total_solde = select([func.sum(Ecriture.dc*EcritureCategorie.montant).label('solde')]).where(
+                         and_(Ecriture.compte_id==compte_id,
+                              Ecriture.id==EcritureCategorie.ecriture_id))
+    print U"Solde : %2.2f €" % (session.query(total_solde).one())
+
+def liste_last(session, compte_id, latest=10):
+    ecritures = session.query(Ecriture).filter_by(compte_id = compte_id).order_by(desc(Ecriture.date_ecriture)).limit(latest)
+    for ecriture in ecritures:
+        print U"%s : %s - %2.2f €, %s" % (ecriture.date_ecriture,
+                                ecriture.type_ecriture,
+                                ecriture.categories[0].montant,
+                                ecriture.nom)
+    
+def main():
+    options = arg_parse()
+
+    engine = create_engine('sqlite:///db/compta.test')
+    
+    #Base = declarative_base()
+    #Base.metadata.bind = engine
+    #Base.metadata.create_all(engine)
+    #banque = session.query(Banque).all()
+    #for titi in banque:
+    #    print tite.nom
+    #    print titi.compte
+    
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+                                                  
+    if options.compte:
+        liste_compte(session)
+    if options.banque:
+        liste_banque(session)
+    if options.solde:
+        liste_solde(session,options.compte_index)
+    if options.last:
+        liste_last(session,options.compte_index)
+
+if __name__ == "__main__":
+    main()
