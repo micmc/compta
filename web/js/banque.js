ea30e7b954a4909d7e57350e676c1fc3e334d8b7
David Micallef <david.micallef@bull.net>
2015-04-14 11:56:11 +0200

    Begin ajax code for list

--- web/js/banque.js
+++ web/js/banque.js
@@ -3,32 +3,36 @@
 $('document').ready(function(){
     //$("table.banque").load();
     //Na marche pas !!!
-    $("table tbody").find("tr:gt(0)").remove();
-    test = $("table tbody").find("tr:gt(0)");
-    alert(test.length);
+    //$("table tbody").find("tr:gt(0)").remove();
+    //test = $("table tbody").find("tr:gt(0)");
+    //alert(test.length);
+    //$("table").selectable({ distance: 1, filter: "tr" });
+    //$("table tbody tr").click(function(event) {
+    //    alert("test");
+    //});
 });
 
-//$("table.banque").ready(function(e) {
-//    $.ajax(
-//    {
-//        method: "GET",
-//        url: "http://localhost:8080/banque",
-//        dataType: "json", 
-//        success: function(data) {
-//            for(i=0; i< data.length; i++) {
-//                $('<tr id="r_'+data[i].id+'">'+
-//                  '<td>'+data[i].nom+'</td>'+
-//                  '<td>'+data[i].adresse+'</td>'+
-//                  '<td>'+data[i].ville+'</td>'+
-//                  '<td>'+data[i].cp+'</td>'+
-//                  '</tr>').appendTo("table.banque tbody");
-//            }
-//        },
-//    });    
-//});
+$("table.banque").ready(function(e) {
+    $.ajax(
+    {
+        method: "GET",
+        url: "http://localhost:8080/banque",
+        dataType: "json", 
+        success: function(data) {
+            for(i=0; i< data.length; i++) {
+                $('<tr id="r_'+data[i].id+'">'+
+                  '<td class="nom">'+data[i].nom+'</td>'+
+                  '<td class="adresse">'+data[i].adresse+'</td>'+
+                  '<td class="ville">'+data[i].ville+'</td>'+
+                  '<td class="cp">'+data[i].cp+'</td>'+
+                  '</tr>').appendTo("table.banque tbody");
+                  $("#r_" + data[i].id).click(function(event) {
+                      var attr_nom = $("td.nom", this);
+                      attr_nom.html('<form><input type="text"></form>');
+                      $(this).off('click');
+                  });
+            }
+        },
+    });    
+});
 
-// Ne marche pas aussi
-$($("#banque").on("Click", "tr", function(e) {
-    //var edittrid = $(this).attr('id');
-    alert("test");
-}));
