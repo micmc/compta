b26c11bf71d713faa73361c1fdb7b7eeb407070b
David Micallef <david.micallef@bull.net>
2015-04-15 15:16:03 +0200

    Finish banque on server

--- server.py
+++ server.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
+""" Application to create server for compta """
 
 from bottle import Bottle
-from bottle import get, run, route, response, request, abort
+from bottle import response, request, abort
 from json import dumps, loads
-from bottle import HTTPError
 from bottle.ext import sqlalchemy
 #from sqlalchemy import create_engine, Column, Integer, Sequence, String
 #from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm.exc import NoResultFound
 
-from compta.db.banque import Banque
-from compta.db.compte import Compte
-from compta.db.ecriture import Ecriture, EcritureCategorie
-from compta.db.categorie import Categorie
 from compta.db.base import Base
+from compta.db.banque import Banque
+#from compta.db.compte import Compte
+#from compta.db.ecriture import Ecriture, EcritureCategorie
+#from compta.db.categorie import Categorie
 
 app = Bottle()
 
 def main():
-    #Base = declarative_base()
+    """ Main Page """
     engine = create_engine('sqlite:///./db/compta.test', echo=False)
 
     plugin = sqlalchemy.Plugin(engine, Base.metadata, create=False)
@@ -31,12 +31,13 @@ def main():
 def enable_cors():
     response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token' 
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
 
 @app.get('/banque')
 @app.get('/banque/<id:int>')
 @app.get('/banque/<nom:re:[a-zA-Z\ ]+>')
-def show(db, id=None, nom=None):
+def list_banque(db, id=None, nom=None):
+    """ Display information for banque """
     banques = db.query(Banque)
     if id:
         banques = banques.filter(Banque.id == id)
@@ -45,7 +46,7 @@ def show(db, id=None, nom=None):
     try:
         banques = banques.order_by(Banque.nom).\
                           all()
-    except NoResultFound :
+    except NoResultFound:
         abort(404, "ID not found")
     if not banques:
         abort(404, "ID not found")
@@ -64,9 +65,40 @@ def show(db, id=None, nom=None):
 
 @app.put('/banque/<id>')
 def update_banque(db, id=None):
-    dir(app)
+    """ Update information for a banque """
     if not id:
         abort(404, 'no id received')
+    data = request.body.readline()
+    if not data:
+        abort(204, 'No data received')
+    entity = loads(data)
+    try:
+        banque = db.query(Banque).\
+                    filter(Banque.id == id).\
+                    one()
+    except NoResultFound:
+        abort(404, "ID not found")
+    if entity.has_key('nom'):
+        banque.nom = entity["nom"]
+    if entity.has_key('adresse'):
+        banque.adresse = entity["adresse"]
+    if entity.has_key('ville'):
+        banque.ville = entity["ville"]
+    if entity.has_key('cp'):
+        banque.cp = entity["cp"]
+    if entity.has_key('pays'):
+        banque.pays = entity["pays"]
+    if entity.has_key('cle'):
+        banque.cle_controle = entity["cle"]
+    if entity.has_key('code_banque'):
+        banque.code_banque = entity["code_banque"]
+    if entity.has_key('code_guichet'):
+        banque.code_guichet = entity["code_guichet"]
+    db.commit()
+
+@app.post('/banque')
+def insert_banque(db):
+    """ Insert a new banque """
     data = request.body.readline()
     if not data:
         abort(204, 'No data received')
@@ -87,35 +119,41 @@ def update_banque(db, id=None):
         entity['code_banque'] = ""
     if not entity.has_key('code_guichet'):
         entity['code_guichet'] = ""
-    banque = db.query(Banque).\
-                filter(Banque.id == id).\
-                one()
-    banque.nom = entity["nom"]
-    banque.adresse = entity["adresse"]
-    banque.ville = entity["ville"]
-    banque.cp = entity["cp"]
-    banque.pays = entity["pays"]
-    banque.cle_controle = entity["cle"]
-    banque.code_banque = entity["code_banque"]
-    banque.code_guichet = entity["code_guichet"]
+    banque = Banque(nom=entity["nom"],
+                    adresse=entity["adresse"],
+                    ville=entity["ville"],
+                    cp=entity["cp"],
+                    pays=entity["pays"],
+                    cle_controle=entity["cle"],
+                    code_banque=entity["code_banque"],
+                    code_guichet=entity["code_guichet"])
+    db.add(banque)
+    db.commit()
+    response.status = 201
+    response.headers["Location"] = "/banque/%s" % (banque.id,)
+
+@app.delete('/banque/<id>')
+def delete_banque(db, id=None):
+    """ Delete a banque """
+    try:
+        banque = db.query(Banque).\
+                    filter(Banque.id == id).\
+                    one()
+    except NoResultFound:
+        abort(404, "ID not found")
+    db.delete(banque)
     db.commit()
 
-#@app.get('/banque/<nom>')
-#def show(db, nom=None):
-#    banque = db.query(Banque).\
-#                filter(Banque.nom.ilike("%%%s%%" % (nom,))).\
-#                first()
-#    return dumps({'id': banque.id, 'nom': banque.nom})
 
-@app.get('/compte/<nom>')
-def show(nom, db):
-    compte = db.query(Compte).\
-                filter(Compte.nom.ilike("%%%s%%" % (nom,))).\
-                first()
-    print compte
-    if compte:
-        return {'id': compte.id, 'nom': compte.nom}
-    return HTTPError(404, 'Entity not found.')
+#@app.get('/compte/<nom>')
+#def show(nom, db):
+#    compte = db.query(Compte).\
+#                filter(Compte.nom.ilike("%%%s%%" % (nom,))).\
+#                first()
+#    print compte
+#    if compte:
+#        return {'id': compte.id, 'nom': compte.nom}
+#    return HTTPError(404, 'Entity not found.')
 
 if __name__ == "__main__":
     main()
