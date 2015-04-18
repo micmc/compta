fc77e8cbee392bc0a69fbd8974db2a82bab52645
David Micallef <david@micallef.fr>
2015-04-18 21:15:53 +0200

    update some bug

--- migrate_sql.py
+++ migrate_sql.py
@@ -4,6 +4,7 @@
 import optparse
 import sys
 #import os
+from decimal import Decimal
 from xml.etree.ElementTree import ElementTree
 from datetime import datetime
 
@@ -47,6 +48,16 @@ def main():
         print "options -i is missing"
         sys.exit(1)
 
+    #Check if compte exist
+    try:
+        compte = session.query(Compte).\
+                         filter(Compte.id == options.compte_index).\
+                         one()
+    except NoResultFound:
+        print "Compte inexistant"
+        sys.exit(1)
+    print "migration pour le compte %s" % (compte.nom,)
+
     #Read xml file
     tree = ElementTree()
     tree.parse(options.file_import)
@@ -54,7 +65,7 @@ def main():
     #compte = Compte(pk=options.compte_index)
     tmp_cpt = 1
     for xml_line in xml_lines:
-        import_date = xml_line.find('FA_Date').split('T')[0]
+        import_date = xml_line.find('FA_Date')
         import_dc = xml_line.find('FA_DC')
         import_type = xml_line.find('FA_MPaiement')
         import_nom = xml_line.find('FA_Nom')
@@ -63,21 +74,21 @@ def main():
         import_montant = xml_line.find('FA_Montant')
         #Write Ecriture
         ecriture = Ecriture(nom=import_nom.text,
-                            date=datetime.strptime(import_date, "%Y/%m/%d"),
+                            date=datetime.strptime(import_date.text.split('T')[0], "%Y-%m-%d"),
                             dc=import_dc.text,
                             type=import_type.text,
-                            compte_id=compte)
+                            compte_id=compte.id)
         session.add(ecriture)
         session.commit()
         #Write EcritureCategorie
-        ecriture_categorie = EcritureCategorie(montant=(int(import_montant.text)*100),
+        ecriture_categorie = EcritureCategorie(montant=int(Decimal(import_montant.text)*100),
                                                ecriture_id=ecriture.id)
         try:
             categorie = session.query(Categorie.id).\
                                      filter(Categorie.nom == import_categorie.text).\
                                      one()
         except NoResultFound:
-            categorie = Categorie(nom=import_categorie)
+            categorie = Categorie(nom=import_categorie.text)
             session.add(categorie)
             session.commit()
         ecriture_categorie.categorie_id = categorie.id
