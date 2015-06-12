0bb62563d4c8a628e6300be1ddc4ec6a0d59b43b
David Micallef <david.micallef@bull.net>
2015-06-12 15:32:14 +0200

    Improve liste ecriture with select area et sum

---
+++ web/js/index.js
@@ -0,0 +1,51 @@
+// Initialisation du Document
+// On efface toutes les lignes du tableau
+
+var id_compte;
+
+$('document').ready(function(){
+    //$("table.banque").load();
+    //Na marche pas !!!
+    //$("table tbody").find("tr:gt(0)").remove();
+    //test = $("table tbody").find("tr:gt(0)");
+    //alert(test.length);
+    //$("table").selectable({ distance: 1, filter: "tr" });
+    //$("table tbody tr").click(function(event) {
+    //    alert("test");
+    //});
+    id_compte = $(location).attr("search").replace("?id=","");
+});
+
+$('select[name="compte"]').ready(function(e) {
+    $.ajax(
+    {
+        method: "GET",
+        url: "http://localhost:8080/compte?filter=prs&archive=no",
+        dataType: "json",
+        success: function(data) {
+            for(i=0; i< data.length; i++) {
+                $('<option value="'+data[i].id+'">'+
+                     data[i].nom+
+                  '</option>'
+                 ).appendTo('select[name="compte"]');
+            }
+            $('select[name="compte"]').change()
+        },
+    });
+});
+
+$('select[name="compte"]').change(function(e) {
+    var compte_id = $(this).val();
+    $("#ref_ecriture").attr("href", "liste_ecriture.html?id=" + compte_id);
+    $.ajax(
+    {
+        method: "GET",
+        url: "http://localhost:8080/compte/" + compte_id + "/ecriture?filter=sum",
+        dataType: "json",
+        success: function(data) {
+            $('#somme').text('Solde du compte : ' + data.somme + "€");
+        },
+    })
+});
+
+
